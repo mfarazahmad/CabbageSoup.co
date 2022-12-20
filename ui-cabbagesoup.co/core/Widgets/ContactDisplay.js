2931f65d509a1485dc9773a8a30702f07a7d731d
Faraz Ahmad <farazahmad@Farazs-MacBook-Pro.local>
2022-12-19 19:22:08 -0500

    Updating Contact Info

--- ui-cabbagesoup.co/core/Widgets/ContactDisplay.js
+++ ui-cabbagesoup.co/core/Widgets/ContactDisplay.js
@@ -6,7 +6,7 @@ function ContactDisplay() {
         <div className="contact">
             <div className="address"><a href="/" rel="external" target="_blank">6595 Roswell Road Suite G-5984</a></div>
             <div className="address"><a href="/" rel="external" target="_blank">Atlanta, GA 30328</a></div>
-            <div ><a href="tel:864-756-1546" rel="external" className="phone">864-756-1546</a></div>
+            <div ><a href="tel:678-753-6320" rel="external" className="phone">678-753-6320</a></div>
             <div ><a href="mailto:contact@brand.com" rel="external" className="mail">contact@cabbagesoup.co</a></div>
         </div>
    );
